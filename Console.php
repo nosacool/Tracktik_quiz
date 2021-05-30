90a874569759c62fe7fc05ad64b6ec77c2e951dd
nosacool <nosacool2007@yahoo.com>
2021-05-30 18:18:45 +0100

    git init

---
+++ Console.php
@@ -0,0 +1,55 @@
+<?php
+require_once('ElectronicItem.php');
+require_once('ElectronicItems.php');
+require_once('AdditionalFunctions_interface.php');
+
+Class Console extends ElectronicItem implements AdditionalFunctions{  // Extends Base Class and Implements Interphase AdditionalFunctions
+
+private $extras = array();
+private $numOfExtras = 0 ;
+const AllowedNumOfExtras = 4; // Set number of 
+
+public function __construct()
+ {
+
+   $this->setType('console');
+ }
+
+public function maxExtras(){
+        if(($this->numOfExtras < self::AllowedNumOfExtras)){
+            return false; // Has not maxed out number of extras allowed so can proceed to add extras.
+        }
+        return true;  //// Has maxed out number of extras allowed so cannot proceed to add extras.
+}
+
+public function addExtras(ElectronicItem $extra){
+    if(!$this->maxExtras()){
+        $this->extras[] = $extra;
+        $this->price = $this->price + $extra->getPrice();
+        $this->numOfExtras = $this->getTotalNumOfExtraItems();
+        return true;
+    }
+    throw new Exception("Exceeded Limit of Extras", 1);
+    return false;
+}
+
+
+private function getTotalNumOfExtraItems(){
+   
+    return sizeof($this->extras);
+}
+
+public function getTotalPriceOfExtras(){
+    $totalPrice = 0;
+    foreach($this->extras as $extra){
+       $totalPrice +=$extra->getPrice();
+    }
+    return $totalPrice;
+}
+
+
+}
+
+
+
+?>
